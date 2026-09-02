cbfb272d0a901a4dd58cf6b1a678e4582f6aabce
Evan Miranda <evanrock520l@ciencias.unam.mx>
2026-09-01 19:39:19 -0600

    chore(client): corrige nombre de la función deserialize

--- server/src/protocol/serializer.rs
+++ server/src/protocol/serializer.rs
@@ -5,7 +5,7 @@ pub fn serialize(msg: &ServerMessage) -> Result<String, serde_json::Error> {
     serde_json::to_string(msg)
 }
 
-pub fn deserealize(data: &str) -> Result<ClientMessage, serde_json::Error> {
+pub fn deserialize(data: &str) -> Result<ClientMessage, serde_json::Error> {
     serde_json::from_str::<ClientMessage>(data)
 }
 
@@ -22,7 +22,7 @@ mod tests {
     #[test]
     fn test_deserealize_identify_message() {
         let data = r#"{"type":"IDENTIFY","username":"Evan"}"#;
-        let msg = deserealize(data).unwrap();
+        let msg = deserialize(data).unwrap();
         assert_eq!(TypeC2S::Identify, msg.type_c2s);
         assert_eq!(Some("Evan".to_string()), msg.username);
     }
@@ -30,7 +30,7 @@ mod tests {
     #[test]
     fn test_deserealize_status_message() {
         let data = r#"{"type":"STATUS","status":"AWAY"}"#;
-        let msg = deserealize(data).unwrap();
+        let msg = deserialize(data).unwrap();
         assert_eq!(TypeC2S::Status, msg.type_c2s);
         assert_eq!(Some(Away), msg.status);
     }
@@ -38,14 +38,14 @@ mod tests {
     #[test]
     fn test_deserealize_users_message() {
         let data = r#"{"type":"USERS"}"#;
-        let msg = deserealize(data).unwrap();
+        let msg = deserialize(data).unwrap();
         assert_eq!(TypeC2S::Users, msg.type_c2s);
     }
 
     #[test]
     fn test_deserealize_text_message() {
         let data = r#"{"type":"TEXT","username":"Evan","text":"Pasa las pruebas"}"#;
-        let msg = deserealize(data).unwrap();
+        let msg = deserialize(data).unwrap();
         assert_eq!(TypeC2S::Text, msg.type_c2s);
         assert_eq!(Some("Evan".to_string()), msg.username);
         assert_eq!(Some("Pasa las pruebas".to_string()), msg.text);
@@ -54,7 +54,7 @@ mod tests {
     #[test]
     fn test_deserealize_public_text_message() {
         let data = r#"{"type":"PUBLIC_TEXT","text":"Pasa las pruebas"}"#;
-        let msg = deserealize(data).unwrap();
+        let msg = deserialize(data).unwrap();
         assert_eq!(TypeC2S::PublicText, msg.type_c2s);
         assert_eq!(Some("Pasa las pruebas".to_string()), msg.text);
     }
@@ -62,7 +62,7 @@ mod tests {
     #[test]
     fn test_deserealize_new_room_message() {
         let data = r#"{"type":"NEW_ROOM","roomname":"Sala 1"}"#;
-        let msg = deserealize(data).unwrap();
+        let msg = deserialize(data).unwrap();
         assert_eq!(TypeC2S::NewRoom, msg.type_c2s);
         assert_eq!(Some("Sala 1".to_string()), msg.room);
     }
@@ -70,7 +70,7 @@ mod tests {
     #[test]
     fn test_deserealize_invite_message() {
         let data = r#"{"type":"INVITE","roomname":"Sala 1","usernames":["Derek","Yahel","Erik"]}"#;
-        let msg = deserealize(data).unwrap();
+        let msg = deserialize(data).unwrap();
 
         assert_eq!(TypeC2S::Invite, msg.type_c2s);
         assert_eq!(Some("Sala 1".to_string()), msg.room);
@@ -87,7 +87,7 @@ mod tests {
     #[test]
     fn test_deserealize_join_room_message() {
         let data = r#"{"type":"JOIN_ROOM","roomname":"Sala 1"}"#;
-        let msg = deserealize(data).unwrap();
+        let msg = deserialize(data).unwrap();
 
         assert_eq!(TypeC2S::JoinRoom, msg.type_c2s);
         assert_eq!(Some("Sala 1".to_string()), msg.room);
@@ -96,7 +96,7 @@ mod tests {
     #[test]
     fn test_deserealize_room_users_message() {
         let data = r#"{"type":"ROOM_USERS","roomname":"Sala 1"}"#;
-        let msg = deserealize(data).unwrap();
+        let msg = deserialize(data).unwrap();
 
         assert_eq!(TypeC2S::RoomUsers, msg.type_c2s);
         assert_eq!(Some("Sala 1".to_string()), msg.room);
@@ -105,7 +105,7 @@ mod tests {
     #[test]
     fn test_deserealize_room_text_message() {
         let data = r#"{"type":"ROOM_TEXT","roomname":"Sala 1","text":"Pasa las pruebas"}"#;
-        let msg = deserealize(data).unwrap();
+        let msg = deserialize(data).unwrap();
 
         assert_eq!(TypeC2S::RoomText, msg.type_c2s);
         assert_eq!(Some("Sala 1".to_string()), msg.room);
@@ -115,7 +115,7 @@ mod tests {
     #[test]
     fn test_deserealize_leave_room_message() {
         let data = r#"{"type":"LEAVE_ROOM","roomname":"Sala 1"}"#;
-        let msg = deserealize(data).unwrap();
+        let msg = deserialize(data).unwrap();
 
         assert_eq!(TypeC2S::LeaveRoom, msg.type_c2s);
         assert_eq!(Some("Sala 1".to_string()), msg.room);
@@ -124,7 +124,7 @@ mod tests {
     #[test]
     fn test_deserealize_disconnect_message() {
         let data = r#"{"type":"DISCONNECT"}"#;
-        let msg = deserealize(data).unwrap();
+        let msg = deserialize(data).unwrap();
 
         assert_eq!(TypeC2S::Disconnect, msg.type_c2s);
     }
