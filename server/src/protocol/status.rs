18cbf9529ea7139bd3a4331512fda8fbf68afc0c
Evan Miranda <evanrock520l@ciencias.unam.mx>
2026-09-01 19:28:48 -0600

    fix(server): añade serialización del enum Status con estilo screaming snake case

--- server/src/protocol/status.rs
+++ server/src/protocol/status.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Debug, PartialEq, Deserialize)]
-#[serde(rename = "INVALID")]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum Status {
     Active,
     Away,
