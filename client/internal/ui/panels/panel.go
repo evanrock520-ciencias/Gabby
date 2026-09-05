85ba8a4de0afcf54e15997961a2e44dfaba4d151
Evan Miranda <evanrock520l@ciencias.unam.mx>
2026-09-05 17:38:34 -0600

    feat(client): añade el tipo panel y layout para hacer composición en los otros paneles

--- client/internal/ui/panels/panel.go
+++ client/internal/ui/panels/panel.go
@@ -5,3 +5,27 @@ package panels
 type InputCapturer interface {
 	IsCapturingInput() bool
 }
+
+type Layout struct {
+	width  int
+	height int
+}
+
+func (layout *Layout) SetSize(width int, height int) {
+	layout.width = width
+	layout.height = height
+}
+
+type Panel struct {
+	Layout
+	focus  bool
+	cursor int
+}
+
+func (panel *Panel) SetFocus(value bool) {
+	panel.focus = value
+}
+
+func (panel *Panel) IsFocused() bool {
+	return panel.focus
+}
