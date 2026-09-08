88a4d82038e992ebbfbcb0f1ae9ca0b12cf957eb
Evan Miranda <evanrock520l@ciencias.unam.mx>
2026-09-07 20:52:49 -0600

    feat(client): añade viewport para visualizar los mensajes del chat

--- client/internal/ui/panels/chat.go
+++ client/internal/ui/panels/chat.go
@@ -1,17 +1,23 @@
 package panels
 
 import (
+	"strings"
+
+	"client/internal/domain"
 	"client/internal/ui/styles"
 
 	"github.com/charmbracelet/bubbles/textinput"
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 )
 
 type ChatModel struct {
 	Panel
-	TextInput textinput.Model
-	ChatName  string
+	TextInput     textinput.Model
+	ChatName      string
+	Viewport      viewport.Model
+	DisplayedRoom domain.Room
+	ready         bool
 }
 
 func NewChatModel() ChatModel {
@@ -20,8 +26,8 @@ func NewChatModel() ChatModel {
 	ti.Blur()
 	ti.CharLimit = 156
 	return ChatModel{
-		TextInput: ti,
-		ChatName:  "Global",
+		TextInput:     ti,
+		DisplayedRoom: domain.NewRoom("Global"),
 	}
 }
 
@@ -29,11 +35,47 @@ func (m ChatModel) Init() tea.Cmd {
 	return textinput.Blink
 }
 
+func (m *ChatModel) SetSize(width int, height int) {
+	m.Panel.SetSize(width, height)
+
+	viewportWidth := max(0, width-4)
+	viewportHeight := max(0, height-4)
+
+	if !m.ready {
+		m.Viewport = viewport.New(viewportWidth, viewportHeight)
+		m.ready = true
+	} else {
+		m.Viewport.Width = viewportWidth
+		m.Viewport.Height = viewportHeight
+	}
+}
+
+func (m *ChatModel) AddMessage(msg domain.ChatMessage) {
+	m.DisplayedRoom.AddMessage(msg)
+	if m.ready {
+		var sb strings.Builder
+		for _, msg := range m.DisplayedRoom.Messages {
+			userRender := styles.HeaderTitleStyle.Render(" " + msg.Username)
+			msgRender := styles.TextStyle.Render(msg.Message)
+			sb.WriteString(styles.MessageStyle.Render(userRender + "\n " + msgRender))
+			sb.WriteString("\n")
+		}
+
+		m.Viewport.SetContent(sb.String())
+		m.Viewport.GotoBottom()
+	}
+}
+
 func (m ChatModel) IsCapturingInput() bool {
 	return m.TextInput.Focused()
 }
 
 func (m ChatModel) Update(msg tea.Msg) (ChatModel, tea.Cmd) {
+	var (
+		textinputCmd tea.Cmd
+		viewportCmd  tea.Cmd
+	)
+
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.Type {
@@ -45,32 +87,47 @@ func (m ChatModel) Update(msg tea.Msg) (ChatModel, tea.Cmd) {
 				cmd := m.TextInput.Focus()
 				return m, cmd
 			}
-			// text := m.TextInput.Value()
+			text := strings.TrimSpace(m.TextInput.Value())
+			// TODO: Sincronizar el username con el usuario del cliente
+			chatMessage := domain.ChatMessage{Message: text, Username: "Evan"}
+			if text != "" {
+				m.AddMessage(chatMessage)
+			}
 			m.TextInput.SetValue("")
 			return m, nil
 		}
 	}
 
-	var cmd tea.Cmd
 	if m.TextInput.Focused() {
-		m.TextInput, cmd = m.TextInput.Update(msg)
+		m.TextInput, textinputCmd = m.TextInput.Update(msg)
+	} else {
+		m.Viewport, viewportCmd = m.Viewport.Update(msg)
 	}
-	return m, cmd
+
+	if _, ok := msg.(tea.MouseMsg); ok {
+		m.Viewport, viewportCmd = m.Viewport.Update(msg)
+	}
+
+	return m, tea.Batch(textinputCmd, viewportCmd)
 }
 
 func (m ChatModel) View() string {
 	boxStyle := styles.BoxStyle.Width(m.width).Height(m.height)
 	header := styles.ChatNameStyle.Width(m.width - 2).Render(m.ChatName)
-	contentStyle := lipgloss.NewStyle().Width(m.width - 2).Height(m.height - 4)
 
 	if m.IsFocused() {
 		boxStyle = boxStyle.BorderForeground(styles.PrimaryColor)
 	}
 
-	messageBarStyle := styles.TextInputStyle.Width(m.width - 4) // -4 Por los bordes de el panel y del input.
+	messageBarStyle := styles.TextInputStyle.Width(m.width - 4) // -4 Por los bordes del panel y del input.
 	if m.IsCapturingInput() {
 		messageBarStyle = messageBarStyle.BorderForeground(styles.SecondaryColor)
 	}
 
-	return boxStyle.Render(header + "\n" + contentStyle.Render() + "\n" + messageBarStyle.Render(m.TextInput.View()))
+	viewportContent := m.Viewport.View()
+	if !m.ready {
+		viewportContent = ""
+	}
+
+	return boxStyle.Render(header + "\n" + viewportContent + "\n" + messageBarStyle.Render(m.TextInput.View()))
 }
