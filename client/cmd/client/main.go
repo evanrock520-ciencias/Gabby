ec0408465ba0f33abd1455552886530fdcef14cf
Evan Miranda <evanrock520l@ciencias.unam.mx>
2026-09-08 22:12:31 -0600

    chore(client): crea un placeholder de Session para utilizarlo en el modelo principal

--- client/cmd/client/main.go
+++ client/cmd/client/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"client/internal/domain"
+	"client/internal/protocol"
 	"client/internal/ui"
 	"fmt"
 
@@ -8,7 +10,16 @@ import (
 )
 
 func main() {
-	p := tea.NewProgram(ui.NewModel(), tea.WithAltScreen())
+	//TODO: Obtener los datos del servidor
+	users := []domain.User{{Username: "Evan", Status: protocol.ACTIVE}, {Username: "Derek", Status: protocol.AWAY}}
+	session := domain.SessionState{
+		Users: users,
+		Rooms: map[string]*domain.Room{
+			"Global": {Name: "Global", Users: users, Messages: []domain.ChatMessage{}},
+		},
+		DMs: make(map[string]*domain.Room),
+	}
+	p := tea.NewProgram(ui.NewModel(session), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Println(err)
 	}
