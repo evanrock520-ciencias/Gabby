c5ee94427cc10f183d883a9a2c214b44b54e087f
Evan Miranda <evanrock520l@ciencias.unam.mx>
2026-09-08 22:09:08 -0600

    feat(client): integra acciones de los paneles lista y modal
    - Integra un panel modal para registro de username
    - Añade un SessionState al model para mostrar el estado de la sesión
    actual.
    - Añade respuestas de la interfaz a los mensajes de los paneles modales
    y de lista.

--- client/internal/ui/model.go
+++ client/internal/ui/model.go
@@ -1,7 +1,9 @@
 package ui
 
 import (
+	"client/internal/domain"
 	"client/internal/protocol"
+	"client/internal/ui/messages"
 	"client/internal/ui/panels"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -37,10 +39,11 @@ type Model struct {
 	chatModel   panels.ChatModel
 	footerModel panels.FooterModel
 	activeModal *panels.ModalModel
+	session     domain.SessionState
 }
 
 func (m Model) Init() tea.Cmd {
-	return m.chatModel.Init()
+	return tea.Batch(m.chatModel.Init(), m.activeModal.TextInput.Focus())
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -61,6 +64,55 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 		if m.activeModal != nil {
 			m.activeModal.SetSize(m.width, max(0, m.heigth-3)) // -3 por el footer
+		}
+
+	case messages.ListResultMsg:
+		switch result := msg.(type) {
+		case messages.EnterRoomMsg:
+			if result.Roomname != "" {
+				if room, ok := m.session.Rooms[result.Roomname]; ok {
+					m.chatModel.SetRoom(room)
+				}
+			}
+		case messages.EnterDMMsg:
+			if result.Username != "" {
+				dmRoom, ok := m.session.DMs[result.Username]
+				if !ok {
+					newDMRoom := domain.NewRoom("@" + result.Username)
+					dmRoom = &newDMRoom
+					if m.session.DMs == nil {
+						m.session.DMs = make(map[string]*domain.Room)
+					}
+					m.session.DMs[result.Username] = dmRoom
+				}
+				m.chatModel.SetRoom(dmRoom)
+			}
+		}
+
+	case messages.ModalResultMsg:
+		m.activeModal = nil
+		m.footerModel.Keymaps = defaultKeymaps
+		m.syncFocus()
+
+		switch result := msg.(type) {
+		case messages.CreateRoomMsg:
+			if result.Roomname != "" {
+				m.roomsModel.Items = append(m.roomsModel.Items, result.Roomname)
+				if m.session.Rooms == nil {
+					m.session.Rooms = make(map[string]*domain.Room)
+				}
+				newRoom := domain.NewRoom(result.Roomname)
+				m.session.Rooms[result.Roomname] = &newRoom
+			}
+		case messages.SetUserMsg:
+			if result.Username != "" {
+				m.footerModel.Username = result.Username
+				m.session.CurrentUser.Username = result.Username
+				m.session.CurrentUser.Status = protocol.ACTIVE
+				m.chatModel.SetUsername(result.Username)
+			} else {
+				return m, tea.Quit
+			}
 		}
 
 	case tea.KeyMsg:
@@ -95,7 +147,8 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 
 		// Modo Navegación
-		return m, m.handleNavegation(msg)
+		cmd := m.handleNavegation(msg)
+		return m, cmd
 
 	default:
 		var cmd tea.Cmd
@@ -156,18 +209,49 @@ func (m Model) View() string {
 }
 
 // Función para poblar los datos de la interfaz
-func NewModel() Model {
-	usersModel := panels.NewListModel("[2] Users", []string{"Yahel", "Derek", "Luis", "Sofia"})
-	roomsModel := panels.NewListModel("[1] Rooms", []string{"Sala 1", "Sala 2", "Sala 3", "Sala 4"})
-	chatModel := panels.NewChatModel()
-	footerModel := panels.FooterModel{Keymaps: defaultKeymaps, Username: "Evan Miranda", Status: protocol.ACTIVE}
+func NewModel(session domain.SessionState) Model {
+	if session.Rooms == nil {
+		session.Rooms = make(map[string]*domain.Room)
+	}
+	if session.DMs == nil {
+		session.DMs = make(map[string]*domain.Room)
+	}
+
+	var users []string
+	for _, user := range session.Users {
+		users = append(users, user.Username)
+	}
+
+	var rooms []string
+	for _, room := range session.Rooms {
+		rooms = append(rooms, room.Name)
+	}
+
+	usersModel := panels.NewListModel("[2] Users", users, func(value string) messages.ListResultMsg {
+		return messages.EnterDMMsg{Username: value}
+	})
+	roomsModel := panels.NewListModel("[1] Rooms", rooms, func(value string) messages.ListResultMsg {
+		return messages.EnterRoomMsg{Roomname: value}
+	})
+	globalRoom, ok := session.Rooms["Global"]
+	if !ok {
+		newGlobal := domain.NewRoom("Global")
+		globalRoom = &newGlobal
+		session.Rooms["Global"] = globalRoom
+	}
+	chatModel := panels.NewChatModel(globalRoom, session.CurrentUser.Username)
+	footerModel := panels.FooterModel{Keymaps: defaultKeymaps, Username: "", Status: protocol.ACTIVE}
+	loginModel := panels.NewModalModel("Login", "Username", 8, func(value string) messages.ModalResultMsg {
+		return messages.SetUserMsg{Username: value}
+	}, false)
 	m := Model{
 		focus:       Rooms,
 		usersModel:  usersModel,
 		roomsModel:  roomsModel,
 		chatModel:   chatModel,
 		footerModel: footerModel,
-		activeModal: nil,
+		activeModal: &loginModel,
+		session:     session,
 	}
 	m.syncFocus()
 	return m
@@ -191,10 +275,14 @@ func (m *Model) handleNavegation(msg tea.KeyMsg) tea.Cmd {
 		m.syncFocus()
 		return m.chatModel.TextInput.Focus()
 	case "c":
-		return m.openModal(panels.NewModalModel("Create Room", "Roomname", 16))
+		return m.openModal(panels.NewModalModel("Create Room", "Roomname", 16, func(value string) messages.ModalResultMsg {
+			return messages.CreateRoomMsg{Roomname: value}
+		}, true))
 	case "i":
 		// TODO: Implementar una secuencia de Modals tipo wizard (porque está selección tiene 2 pasos)
-		return m.openModal(panels.NewModalModel("Invitate", "Roomname", 17))
+		return m.openModal(panels.NewModalModel("Invitate", "Roomname", 17, func(value string) messages.ModalResultMsg {
+			return messages.InvitateMsg{Roomname: value}
+		}, true))
 	default:
 		var cmd tea.Cmd
 		switch m.focus {
