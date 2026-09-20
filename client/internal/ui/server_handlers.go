1ba09bb87ca7fa10b95452f0c7624ce50a98141e
Evan Miranda <evanrock520l@ciencias.unam.mx>
2026-09-19 21:38:42 -0600

    feat(client): añade enrutador para los mensajes del servidor al cliente

---
+++ client/internal/ui/server_handlers.go
@@ -0,0 +1,56 @@
+package ui
+
+import (
+	"client/internal/domain"
+	"client/internal/protocol"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+// routeServerMessage enruta los mensajes del servidor al cliente.
+func (m *Model) routeServerMessage(msg protocol.ServerMessage) tea.Cmd {
+	switch msg.Type {
+	case protocol.RESPONSE:
+		return m.routeResponse(msg)
+	case protocol.USER_LIST:
+		return m.handleUserList(msg)
+	}
+
+	return nil
+}
+
+// handleUserList maneja la lógica de obtener la lista de usuarios.
+func (m *Model) handleUserList(msg protocol.ServerMessage) tea.Cmd {
+	for username, status := range msg.Users {
+		user := domain.User{
+			Username: username,
+			Status:   status,
+		}
+		m.session.Users = append(m.session.Users, user)
+		m.usersModel.AddItem(NewUserItem(user))
+	}
+
+	return nil
+}
+
+// routeResponse enruta los mensajes del servidor al cliente
+// que sean especificamente una respuesta.
+func (m *Model) routeResponse(msg protocol.ServerMessage) tea.Cmd {
+	switch msg.Operation {
+	case protocol.IDENTIFY:
+		m.handleIdentifyResponse(msg)
+
+	}
+	return nil
+}
+
+// handleIdentifyResponse Verifica que hubo una identificación exitosa.
+func (m *Model) handleIdentifyResponse(msg protocol.ServerMessage) tea.Cmd {
+	if msg.Result == protocol.SUCCESS {
+		m.closeModal()
+		m.SetUsername(msg.Extra)
+		return nil
+	}
+
+	return nil
+}
