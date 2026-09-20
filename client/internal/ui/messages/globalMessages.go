2dfe8b2752d1a785a37d93bf04dbcd7ab8cfc129
Evan Miranda <evanrock520l@ciencias.unam.mx>
2026-09-19 18:09:55 -0600

    refactor(client): elimina las interfaces de mensajes

--- client/internal/ui/messages/globalMessages.go
+++ client/internal/ui/messages/globalMessages.go
@@ -2,12 +2,6 @@ package messages
 
 import "client/internal/domain"
 
-type GlobalResultMsg interface {
-	isGlobalResult()
-}
-
 type ChangeStatusMsg struct {
 	Status domain.Status
 }
-
-func (ChangeStatusMsg) isGlobalResult() {}
--- client/internal/ui/messages/listMessage.go
+++ client/internal/ui/messages/listMessage.go
@@ -1,19 +1,9 @@
 package messages
 
-type ListType int
-
-type ListResultMsg interface {
-	isListResult()
-}
-
 type EnterRoomMsg struct {
 	Roomname string
 }
 
-func (EnterRoomMsg) isListResult() {}
-
 type EnterDMMsg struct {
 	Username string
 }
-
-func (EnterDMMsg) isListResult() {}
