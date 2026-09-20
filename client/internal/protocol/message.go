e52d95496a626d3e77d18bba6698a9356a17e37c
Evan Miranda <evanrock520l@ciencias.unam.mx>
2026-09-19 21:33:52 -0600

    fix(client): añade deserealización de las listas de usuarios que manda el cliente

--- client/internal/protocol/message.go
+++ client/internal/protocol/message.go
@@ -3,9 +3,10 @@ package protocol
 import "client/internal/domain"
 
 type Message struct {
-	Username  string        `json:"username,omitempty"`
-	Roomname  string        `json:"roomname,omitempty"`
-	Status    domain.Status `json:"status,omitempty"`
-	Text      string        `json:"text,omitempty"`
-	Usernames []string      `json:"usernames,omitempty"`
+	Username  string                   `json:"username,omitempty"`
+	Roomname  string                   `json:"roomname,omitempty"`
+	Status    domain.Status            `json:"status,omitempty"`
+	Text      string                   `json:"text,omitempty"`
+	Usernames []string                 `json:"usernames,omitempty"`
+	Users     map[string]domain.Status `json:"users,omitempty"`
 }
