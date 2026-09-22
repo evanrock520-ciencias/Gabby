c4995382666c4e07de22243f8f7f32cfa6ede77c
Evan Miranda <evanrock520l@ciencias.unam.mx>
2026-09-22 13:50:00 -0600

    test: añade pruebas básicas de estrés

---
+++ stress/stress.js
@@ -0,0 +1,48 @@
+import { Socket } from "k6/x/tcp";
+
+export const options = {
+  vus: 512
+}
+
+/**
+ * Corre pruebas de estrés con m usuarios. El flujo es registrarse, mandar n mensajes públicos
+ * y desconectarse. Asi, manda m*n mensajes.
+ */
+export default async function () {
+  const host = __ENV.TCP_ECHO_HOST || "localhost";
+  const port = __ENV.TCP_ECHO_PORT || "8080";
+  const id = __VU
+  const messages = 5
+
+
+  const socket = new Socket();
+  const closed = new Promise((resolve) => {
+    socket.on("close", () => {
+      console.log(`"Closed the connection ${id}"`);
+      resolve();
+    });
+  });
+
+  socket.on("error", (err) => {
+    console.error("Error:", err);
+  });
+
+socket.on("data", (data) => {
+   const response = String.fromCharCode.apply(null, new Uint8Array(data));
+   socket.destroy();
+});
+
+  await socket.connect(port, host);
+  console.log(`"User ${id} Connected!"`);
+
+  await socket.write(`{"type":"IDENTIFY", "username":"u${id}"}\n`)
+
+  const writes = [];
+  for (let i = 0; i < messages; i++) {
+    writes.push(socket.write(`{"type":"PUBLIC_TEXT", "text":"u${id} send a message for the ${i} time"}\n`));
+  }
+
+  await Promise.all(writes);
+
+  await closed;
+}
